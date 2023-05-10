a9acc94da1d72257ba4e8067d83a0f7a459e535b
Waqas Dilawar <20771676+waqasdilawar@users.noreply.github.com>
2023-05-10 16:02:20 +0500

    Merge pull request #2
    * Added groupBy and reduce but the output does not look correct
    
    * Implemented aggregation and it's working just fine.
    
    * Added output data to Sample data.json file and removed TODO as it's b…
    
    * Refactored the model names
    
    * Updated sample data

--- src/main/java/com/unifonic/frauddetectionsystem/streams/ProfanityFilterStream.java
+++ src/main/java/com/unifonic/frauddetectionsystem/streams/ProfanityFilterStream.java
@@ -3,12 +3,12 @@ package com.unifonic.frauddetectionsystem.streams;
 import com.unifonic.frauddetectionsystem.model.ProfanityWord;
 import com.unifonic.frauddetectionsystem.model.ProfanityWordCheck;
 import com.unifonic.frauddetectionsystem.model.ProfanityWordSplit;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.kstream.Joined;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Arrays;
 import java.util.function.BiFunction;
@@ -17,15 +17,15 @@ import java.util.function.BiFunction;
 public class ProfanityFilterStream {
 
   @Bean
-  public BiFunction<KStream<String, ProfanityWordCheck>, KTable<String, ProfanityWord>, KStream<String, ProfanityWordResult>> process() {
+  public BiFunction<KStream<String, ProfanityWordCheck>, KTable<String, ProfanityWord>,
+          KStream<String, ProfanityWordResult.ProfanityResultAggregator>> process() {
     return (inputTextStream, blockWordsTable) -> (inputTextStream
             .flatMapValues((readOnlyKey, value) -> Arrays.asList(value.getWord().split("\\s+"))
                     .stream().map(s -> new ProfanityWordSplit(s, value))
                     .toList())
             .map((key, value) -> KeyValue.pair(value.getWord(), value))
-            .join(blockWordsTable, (readOnlyKey, value1, value2) ->
-            new ProfanityWordResult(value1, value2), Joined.valueSerde(new ProfanityWordSplit())
-            )
+            .join(blockWordsTable, (readOnlyKey, value1, value2) -> new ProfanityWordResult(value1, value2),
+                    Joined.valueSerde(new ProfanityWordSplit()))
             .filter((key, value) -> {
               var profanityWord = value.getProfanityWord();
               var profanityCheckText = value.getProfanityCheckText();
@@ -33,6 +33,27 @@ public class ProfanityFilterStream {
                 return Arrays.stream(profanityCheckText.getWord().split("\\s+"))
                         .anyMatch(s -> profanityWord.getWord().equals(s));
               } else return false;
+            })
+            .peek((key, value) -> System.out.println(key + " value:" + value.getProfanityCheckText().getWord()))
+            .toTable(Materialized.with(Serdes.String(), new ProfanityWordResult()))
+            .groupBy((key, value) -> {
+             return KeyValue.pair(value.getProfanityCheckText().getProfanityWordCheck().getWord(),value);
+            }, Grouped.valueSerde(new ProfanityWordResult()))
+            .aggregate(ProfanityWordResult.ProfanityResultAggregator::new, (key, value, aggregate) -> {
+              if (key != null)
+              {
+                aggregate.adder(value);
+              }
+              return aggregate;
+            }, (key, value, agg) -> {
+                      agg.remover(value);
+                      return agg;
+                    },
+                    Materialized.with(Serdes.String(), new ProfanityWordResult.ProfanityResultAggregator()))
+            .toStream()
+            .filter((key, value) -> value != null && !CollectionUtils.isEmpty(value.getProfanityWordResults()))
+            .peek((key, value) -> {
+              System.out.println(key + " value:" + value.getProfanityWordResults().iterator().next().getProfanityCheckText().getWord());
             }));
   }
 }
--- src/main/java/com/unifonic/frauddetectionsystem/streams/ProfanityWordResult.java
+++ src/main/java/com/unifonic/frauddetectionsystem/streams/ProfanityWordResult.java
@@ -2,10 +2,18 @@ package com.unifonic.frauddetectionsystem.streams;
 
 import com.unifonic.frauddetectionsystem.model.ProfanityWord;
 import com.unifonic.frauddetectionsystem.model.ProfanityWordSplit;
+import org.springframework.kafka.support.serializer.JsonSerde;
 
-public class ProfanityWordResult {
-  private final ProfanityWordSplit profanityCheckText;
-  private final ProfanityWord profanityWord;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ProfanityWordResult extends JsonSerde<ProfanityWordResult> {
+  private ProfanityWordSplit profanityCheckText;
+  private ProfanityWord profanityWord;
+
+  public ProfanityWordResult() {
+  }
 
   public ProfanityWordResult(ProfanityWordSplit profanityCheckText, ProfanityWord profanityWord) {
     this.profanityCheckText = profanityCheckText;
@@ -18,5 +26,33 @@ public class ProfanityWordResult {
 
   public ProfanityWord getProfanityWord() {
     return profanityWord;
+  }
+
+  public static class ProfanityResultAggregator extends JsonSerde<ProfanityResultAggregator>
+  {
+    List<ProfanityWordResult> profanityWordResults;
+
+    public ProfanityResultAggregator()
+    {
+      this.profanityWordResults = new ArrayList<>();
+    }
+
+    public void adder(ProfanityWordResult profanityWordResultAdder)
+    {
+      if (profanityWordResultAdder != null)
+      {
+        this.profanityWordResults.add(profanityWordResultAdder);
+      }
+    }
+
+    public void remover(ProfanityWordResult profanityWordResultRemover) {
+      this.profanityWordResults.removeIf(profanityWordResult -> Objects.equals(profanityWordResult.getProfanityCheckText()
+                      .getProfanityWordCheck().getWord(),
+              profanityWordResultRemover.getProfanityCheckText().getProfanityWordCheck().getWord()));
+    }
+
+    public List<ProfanityWordResult> getProfanityWordResults() {
+      return profanityWordResults;
+    }
   }
 }
